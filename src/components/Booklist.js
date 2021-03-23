3af64e5227bd0755ca683a8293e783141b656c2d
葛西　佳祐 <keisuke1350@gmail.com>
2021-03-23 23:16:34 +0900

    0323

---
+++ src/components/Booklist.js
@@ -0,0 +1,30 @@
+// Booklist.jsx
+import React, { useState, useEffect } from "react";
+
+const Booklist = (props) => {
+    const [bookData, setBookData] = useState(null);
+    const result = props.getData?.(props.language);
+
+    useEffect(() => {
+        const result = props
+            .getData?.(props.language)
+            .then((response) => setBookData(response));
+    }, [props])
+
+    return (
+        <div>
+            <ul>
+            {
+                bookData === null ? (
+                    <p>now loading...</p>
+                ) : (
+                    bookData.data.items.map((x, index) => (
+                        <li key={index}>{x.volumeInfo.title}</li>
+                    ))
+                )
+            }
+            </ul>
+        </div>
+    );
+};
+export default Booklist;
